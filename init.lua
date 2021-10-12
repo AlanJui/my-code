b6e66bfaaa9c296c036025d815709d29fd674a28
Alan Jui <alanjui.1960@gmail.com>
2021-10-12 14:39:30 +0800

    變更預設，令文字編輯，遇超過 margin 時，不會自動換行

--- init.lua
+++ init.lua
@@ -67,3 +67,7 @@ require('keymappings')
 -- Misc.
 -- ==========================================================
 -- require('weather').init()
+
+-- Diable line wrap
+vim.opt.textwidth=0
+vim.opt.wrapmargin=0
